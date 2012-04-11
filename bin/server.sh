12baefb3ed44cc4a3c1a7ad73c2386f68bbd547e
winton <mail@wintoni.us>
2012-04-11 10:39:07 -0700

    bin/server.sh fix

--- bin/server.sh
+++ bin/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # If command is 'start' and (development or no env specified)
-if [ $1 = 'start' ] && ([ "$NODE_ENV" = 'development' ] || [ -z "$STATE" ]); then
+if [ $1 = 'start' ] && ([ "$NODE_ENV" = 'development' ] || [ -z "$NODE_ENV" ]); then
 	./node_modules/.bin/supervisor -e 'js|coffee' -w 'server,node_modules' server/app.js
 fi
 
@@ -9,5 +9,5 @@ fi
 if [ $1 = 'start' ] && [ "$NODE_ENV" = 'production' ]; then
 	export NODE_PATH=/usr/lib/node_modules 
 	export NODE_ENV=production
-	PORT=$2 /usr/bin/node server/app.js >> log/production.$1.log 2>&1
+	PORT=$2 /usr/bin/node server/app.js >> log/production.$2.log 2>&1
 fi
